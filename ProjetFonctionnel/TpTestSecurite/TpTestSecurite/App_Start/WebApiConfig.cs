dd9c4ef7a1731263a94e4b6e9c61314510a6e69c
Paul <paulhourcade40@gmail.com>
2018-12-17 09:20:34 +0100

    projet fonctionnel utilisant C#

---
+++ ProjetFonctionnel/TpTestSecurite/TpTestSecurite/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TpTestSecurite
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+            //config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:4200/api/produit","*","**")
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+        }
+    }
+}
